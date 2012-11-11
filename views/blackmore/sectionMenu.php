40ac79ac2689e5bc8fbd129457450b2b69c5e89e
Eser <eser@sent.com>
2012-11-11 14:12:58 -0800

    * blackmore is updated to view each sections' links on sidebar.

---
+++ views/blackmore/sectionMenu.php
@@ -0,0 +1,19 @@
+<?php
+	$tModule = &blackmore::$modules[$model];
+?>
+									<div class="menuDiv">
+										<div class="menuDivHeader"><a class="boxed" href="<?php echo mvc::url('blackmore/' . $model); ?>"><?php echo _($tModule['title']); ?></a></div>
+										<?php if(isset($tModule['submenus']) && $tModule['submenus']) { ?>
+										<ul>
+											<?php
+												foreach($tModule['actions'] as $tSubmenuItem) {
+													if(!isset($tSubmenuItem['menutitle'])) {
+														continue;
+													}
+											?>
+											<li><a class="boxed iconcategoryadd" href="<?php echo mvc::url('blackmore/' . $model . '/' . $tSubmenuItem['action']); ?>"><?php echo _($tSubmenuItem['menutitle']); ?></a></li>
+											<?php } ?>
+										</ul>
+										<?php } ?>
+									</div>
+
