117a0e9b17842d86354193070a646f15a29012ee
Eser <eser@sent.com>
2012-11-29 05:13:07 -0800

    * added view class for views extension. * added views::setRange and view->setRange methods.

--- extensions/views/views.php
+++ extensions/views/views.php
@@ -68,6 +68,15 @@
 		/**
 		 * @ignore
 		 */
+		public static function setRange($uArray) {
+			foreach($uArray as $tKey => &$tValue) {
+				self::$vars[$tKey] = $tValue;
+			}
+		}
+
+		/**
+		 * @ignore
+		 */
 		public static function remove($uKey) {
 			unset(self::$vars[$uKey]);
 		}
@@ -150,6 +159,81 @@
 				views::$viewEngines[$tViewExtension] . '::renderview',
 				$tViewArray
 			);
+		}
+	}
+
+	/**
+	 * View Class
+	 *
+	 * @package Scabbia
+	 * @subpackage LayerExtensions
+	 */
+	class view {
+		/**
+		 * @ignore
+		 */
+		public $path;
+		/**
+		 * @ignore
+		 */
+		public $vars;
+
+		/**
+		 * @ignore
+		 */
+		public function __construct($uPath) {
+			$this->path = &$uPath;
+		}
+
+		/**
+		 * @ignore
+		 */
+		public function get($uKey) {
+			return $this->vars[$uKey];
+		}
+
+		/**
+		 * @ignore
+		 */
+		public function set($uKey, $uValue) {
+			$this->vars[$uKey] = $uValue;
+		}
+
+		/**
+		 * @ignore
+		 */
+		public function setRef($uKey, &$uValue) {
+			$this->vars[$uKey] = & $uValue;
+		}
+
+		/**
+		 * @ignore
+		 */
+		public function setRange($uArray) {
+			foreach($uArray as $tKey => &$tValue) {
+				$this->vars[$tKey] = $tValue;
+			}
+		}
+		
+		/**
+		 * @ignore
+		 */
+		public function remove($uKey) {
+			unset($this->vars[$uKey]);
+		}
+
+		/**
+		 * @ignore
+		 */
+		public function render() {
+			views::view($this->path, $this->vars);
+		}
+
+		/**
+		 * @ignore
+		 */
+		public function renderFile() {
+			views::viewFile($this->path, $this->vars);
 		}
 	}
 
