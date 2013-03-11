d246eabf3702fc97e77eefdf862286596f0c5d75
Eser <eser@sent.com>
2013-03-11 23:34:59 +0200

    * Blackmore is being migrated to bootstrap.

--- views/blackmore/login.php
+++ views/blackmore/login.php
@@ -5,65 +5,55 @@
 	use Scabbia\Extensions\Http\Http;
 ?>
 <?php echo Html::doctype('html5'); ?>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
-<head>
-	<meta charset="utf-8" />
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8" />
 
-	<title><?php echo _(Config::get('blackmore/title', 'Scabbia: Blackmore')); ?></title>
+        <title><?php echo _(Config::get('blackmore/title', 'Scabbia: Blackmore')); ?></title>
 
-	<link type="text/css" href="<?php echo Http::url('scabbia.css?reset,jquery,jqueryui,validation,cleditor,tablesorter,shadowbox,blackmore') ?>" rel="stylesheet" media="all" />
-    <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php echo Http::url('home/rss'); ?>" />
-    <link rel="pingback" href="<?php echo Http::url('api/xmlrpc'); ?>" />
+        <link type="text/css" href="<?php echo Http::url('scabbia.css?core,jquery,validation,bootstrap,cleditor,tablesorter,blackmore') ?>" rel="stylesheet" media="all" />
+        <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php echo Http::url('home/rss'); ?>" />
+        <link rel="pingback" href="<?php echo Http::url('api/xmlrpc'); ?>" />
 
-	<script type="text/javascript" src="<?php echo Http::url('scabbia.js?jquery,jqueryui,validation,cleditor,tablesorter,shadowbox,flot,blackmore'); ?>"></script>
-</head>
-<body class="<?php echo Config::get('blackmore/bodyStyle', 'stretch'); ?> login">
-<script type="text/javascript">
-	$l.contentBegin('login', '<?php echo Framework::$siteroot; ?>');
-</script>
+        <script type="text/javascript" src="<?php echo Http::url('scabbia.js?core,jquery,validation,bootstrap,cleditor,tablesorter,flot,blackmore'); ?>"></script>
+    </head>
+    <body class="<?php echo Config::get('blackmore/bodyStyle', 'stretch'); ?> login">
+        <script type="text/javascript">
+            $l.contentBegin('login', '<?php echo Framework::$siteroot; ?>');
+        </script>
 
-<div id="page">
-	<div class="block small center login">
+        <div class="block">
 
-		<div class="block_head">
-			<div class="bheadl"></div>
-			<div class="bheadr"></div>
+            <div class="block_head">
+                <h2><?php echo _(Config::get('blackmore/loginTitle', 'Scabbia: Blackmore Login')); ?></h2>
+            </div>
 
-			<h2><?php echo _(Config::get('blackmore/loginTitle', 'Scabbia: Blackmore Login')); ?></h2>
-		</div>
+            <div class="block_content">
+                <?php if(isset($error)) { ?>
+                <div class="alert alert-error"><?php echo $error; ?></div>
+                <?php } ?>
 
-		<div class="block_content">
-			<?php if(isset($error)) { ?>
-			<h2><?php echo _('Error:'); ?></h2>
-			<div class="error"><?php echo $error; ?></div>
-			<?php } ?>
+                <form method="POST" action="<?php echo Http::url('blackmore/login'); ?>">
+                    <fieldset>
+                        <div class="indent">
+                            <label><?php echo _('Username:'); ?></label>
+                            <input type="text" class="text" name="username" placeholder="Enter username" />
 
-			<form method="POST" action="<?php echo Http::url('blackmore/login'); ?>">
-				<p>
-					<label><?php echo _('Username:'); ?></label> <br />
-					<input type="text" class="text tipsyFocus" name="username" title="Enter username" />
-				</p>
+                            <label><?php echo _('Password:'); ?></label>
+                            <input type="password" class="text" name="password" placeholder="Enter password" />
+                        </div>
 
-				<p>
-					<label><?php echo _('Password:'); ?></label> <br />
-					<input type="password" class="text tipsyFocus" name="password" title="Enter password" />
-				</p>
+                        <div class="form-actions">
+                            <input type="submit" class="btn btn-primary" value="<?php echo _('Login'); ?>" />
+                        </div>
+                    </fieldset>
+                </form>
 
-				<p>
-					<input type="submit" class="submit" value="<?php echo _('Login'); ?>" name="submit" />
-				</p>
-			</form>
+            </div>
+        </div>
 
-		</div>
-
-		<div class="bendl"></div>
-		<div class="bendr"></div>
-
-	</div>
-</div>
-
-<script type="text/javascript">
-	$l.contentEnd();
-</script>
-</body>
+        <script type="text/javascript">
+            $l.contentEnd();
+        </script>
+    </body>
 </html>
--- views/blackmore/sectionMenu.php
+++ views/blackmore/sectionMenu.php
@@ -5,23 +5,19 @@
 <?php
 	$tModule = & Blackmore::$modules[$model];
 ?>
-<div class="menuDiv">
-	<div class="menuDivHeader"><a class="boxed"
-			href="<?php echo Http::url('blackmore/' . $model); ?>"><?php echo _($tModule['title']); ?></a>
-	</div>
-	<?php if(isset($tModule['submenus']) && $tModule['submenus']) { ?>
-	<ul>
-		<?php
-		foreach($tModule['actions'] as $tSubmenuItem) {
-			if(!isset($tSubmenuItem['menutitle'])) {
-				continue;
-			}
-			?>
-			<li><a class="boxed iconcategoryadd"
-					href="<?php echo Http::url('blackmore/' . $model . '/' . $tSubmenuItem['action']); ?>"><?php echo _($tSubmenuItem['menutitle']); ?></a>
-			</li>
-			<?php } ?>
-	</ul>
-	<?php } ?>
-</div>
+<ul class="nav nav-list">
+	<li class="nav-header"><?php echo _($tModule['title']); ?></li>
+	<?php
+        if(isset($tModule['submenus']) && $tModule['submenus']) {
+		    foreach($tModule['actions'] as $tSubmenuItem) {
+			    if(!isset($tSubmenuItem['menutitle'])) {
+    				continue;
+    			}
+    ?>
+		<li><a href="<?php echo Http::url('blackmore/' . $model . '/' . $tSubmenuItem['action']); ?>"><?php echo _($tSubmenuItem['menutitle']); ?></a></li>
+	<?php
+            }
+        }
+    ?>
+</ul>
 
