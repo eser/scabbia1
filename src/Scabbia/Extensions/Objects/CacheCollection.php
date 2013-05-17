59bb40709f9256e5ebdbc82823fc382dc3713d65
larukedi <eser@sent.com>
2013-05-17 10:07:30 +0300

    * Collection->getRange and CacheCollection->updateRange added (bugfix).

--- src/Scabbia/Extensions/Objects/CacheCollection.php
+++ src/Scabbia/Extensions/Objects/CacheCollection.php
@@ -94,6 +94,6 @@ class CacheCollection extends Collection
         $this->enqueue($uArray);
         $this->update();
 
-        return $this->getRange($uArray);
+        return $this->getRange($this->_items);
     }
 }
