44f2a9c77a56ccc01390053084d551d7d180ebe7
larukedi <eser@sent.com>
2013-06-25 11:15:32 +0300

    * added Binder.

---
+++ src/Scabbia/Binder.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Scabbia Framework Version 1.1
+ * http://larukedi.github.com/Scabbia-Framework/
+ * Eser Ozvataf, eser@sent.com
+ */
+
+namespace Scabbia;
+
+use Scabbia\Io;
+
+/**
+ * Binder combines multiple files into a file.
+ *
+ * @package Scabbia
+ * @version 1.1.0
+ *
+ * @todo ttl per binder instance to invalidate cache
+ * @todo caching
+ */
+class Binder
+{
+    /**
+     * @var array Set of callback methods will be applied to files individually by types.
+     */
+    public static $fileProcessors = array();
+    /**
+     * @var array Set of callback methods will be applied generated pack by type.
+     */
+    public static $packProcessors = array();
+    /**
+     * @var object LESS compiler instance.
+     */
+    public static $lessCompiler = null;
+
+
+    /**
+     * @var array Set of parts.
+     */
+    public $parts = array();
+    /**
+     * @var string Name of the output.
+     */
+    public $outputName;
+    /**
+     * @var string Type of the output.
+     */
+    public $outputType;
+    /**
+     * @var string Class list.
+     */
+    public $classes;
+
+
+    /**
+     * Static initialization of binder class.
+     */
+    public static function init()
+    {
+        self::$fileProcessors['less'] = function ($uInput, $uDescription) {
+            if (is_null(self::$lessCompiler)) {
+                self::$lessCompiler = new \lessc();
+            }
+
+            $tContent  = '/* LESS ' . $uDescription . ' */' . PHP_EOL;
+            $tContent .= self::$lessCompiler->compileFile($uInput);
+            $tContent .= PHP_EOL;
+
+            return $tContent;
+        };
+
+        self::$fileProcessors['css'] = function ($uInput, $uDescription) {
+            $tContent  = '/* CSS ' . $uDescription . ' */' . PHP_EOL;
+            $tContent .= $uInput;
+            $tContent .= PHP_EOL;
+
+            return $tContent;
+        };
+
+        self::$fileProcessors['js'] = function ($uInput, $uDescription) {
+            $tContent  = '/* JS ' . $uDescription . ' */' . PHP_EOL;
+            $tContent .= $uInput;
+            $tContent .= PHP_EOL;
+
+            return $tContent;
+        };
+
+        self::$fileProcessors['php'] = function ($uInput, $uDescription) {
+            return Utils::printFile($uInput);
+        };
+    }
+
+
+    /**
+     * Constructs a new instance of binder.
+     *
+     * @param string $uOutputName Name of the output
+     * @param string $uOutputType Type of the output
+     * @param array  $uClasses Classes
+     */
+    public function __construct($uOutputName, $uOutputType, array $uClasses = array())
+    {
+        $this->outputName = $uOutputName;
+        $this->outputType = $uOutputType;
+        $this->classes = $uClasses;
+    }
+
+    /**
+     * Adds a new part to binder instance.
+     *
+     * @param string $uBindType Binding type, available options are function, string and file
+     * @param string $uPartType Type of the part
+     * @param string $uValue Value
+     * @param string $uClass Class name
+     */
+    public function add($uBindType, $uPartType, $uValue, $uClass = null)
+    {
+        $this->parts[] = array(
+            'bindtype' => $uBindType,
+            'parttype' => $uPartType,
+            'class' => $uClass,
+            'value' => $uValue
+        );
+    }
+
+    /**
+     * Outputs all parts in single output.
+     *
+     * @returns string Output
+     */
+    public function output()
+    {
+        $tContent = '';
+
+        foreach ($this->parts as $tPart) {
+            if (!is_null($tPart['class']) && !in_array($tPart['class'], $this->classes, true)) {
+                continue;
+            }
+
+            if ($tPart['bindtype'] == 'function') {
+                $tValue = call_user_func($tPart['value'], $tPart);
+                $tDescription = 'function ' . $tPart['value'];
+            } elseif ($tPart['bindtype'] == 'string') {
+                $tValue = $tPart['value'];
+                $tDescription = 'string';
+            } else {
+                $tValue = Io::read(Io::translatePath($tPart['value']));
+                $tDescription = 'file ' . $tPart['value'];
+            }
+
+            if (array_key_exists($tPart['parttype'], self::$fileProcessors)) {
+                $tContent .= call_user_func(
+                    self::$fileProcessors[$tPart['parttype']],
+                    $tValue,
+                    $tDescription
+                );
+            } else {
+                $tContent .= $tValue;
+            }
+
+        }
+
+        if (array_key_exists($this->outputType, self::$packProcessors)) {
+            return call_user_func(self::$packProcessors[$this->outputType], $tContent);
+        }
+
+        return $tContent;
+    }
+}
